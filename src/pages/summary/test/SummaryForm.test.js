7690ab04934f8d95bddbda4b0e76a97ffa563f1f
Alessandro Catacchio <a.catacchio5@gmail.com>
2022-09-14 12:36:33 +0200

    userEvent installed and first use

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -1,13 +1,14 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
 const btnName = /confirm order/i
 const checkboxName = /terms and conditions/i
 
-let btn;
-let checkbox;
+let btn, checkbox, user;
 
 beforeEach(() => {
+    user = userEvent.setup()
     render(<SummaryForm />)
     btn = screen.getByRole('button', { name: btnName })
     checkbox = screen.getByRole('checkbox', { name: checkboxName })
@@ -17,16 +18,30 @@ it('Initial condition', () => {
     // Checkbox unchecked  and btn disabled by default
     expect(checkbox).not.toBeChecked();
     expect(btn).toBeDisabled()
-
 })
 
 // it('Checkbox behavior', () => { I wrote 'checkbox behavior', but is too generic? Maybe is better to explain better the behavior
-it('Checkbox enables btn on 1st click and disables on 2nd click', () => {
+it('Checkbox enables btn on 1st click and disables on 2nd click', async () => {
     // Checking checkbox enables button
-    fireEvent.click(checkbox);
+    await userEvent.click(checkbox);
     expect(btn).toBeEnabled();
 
     //Unchecking checkbox disables button
-    fireEvent.click(checkbox);
+    await userEvent.click(checkbox);
     expect(btn).toBeDisabled();
 })
+
+xit("Checkbox enables button on first click and disables on second click", async () => {
+    // const user = userEvent.setup(); // THIS LINE
+    render(<SummaryForm />);
+    const checkbox = screen.getByRole("checkbox", {
+        name: /terms and conditions/i,
+    });
+    const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+
+    await userEvent.click(checkbox);
+    expect(confirmButton).toBeEnabled();
+
+    await userEvent.click(checkbox);
+    expect(confirmButton).toBeDisabled();
+});
